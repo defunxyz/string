c45c38daf535615fdeca7569c61e999f7cd0634d
Fisnik <25180667+defunxyz@users.noreply.github.com>
2023-05-18 14:08:03 +0200

    Added initial code

---
+++ include/string/string.hpp
@@ -0,0 +1,24 @@
+// Copyright (C) Fisnik. All rights reserved.
+#ifndef STRING_HPP
+#define STRING_HPP
+
+#if defined(_MSC_VER) ||                                            \
+    (defined(__GNUC__) && (__GNUC__ == 3 && __GNUC_MINOR__ >= 4) || \
+     (__GNUC__ >= 4))
+#pragma once
+#endif
+
+#define STRING_VERSION ""
+#define STRING_VERSION_MAJOR 0
+#define STRING_VERSION_MINOR 0
+
+namespace detail {
+    class string{
+    public:
+    
+    private:
+    
+    };
+}
+
+#endif
