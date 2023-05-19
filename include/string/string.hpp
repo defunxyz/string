c39e6a56f9f3669c488729b6e8a7f6078c1d162b
Fisnik <25180667+defunxyz@users.noreply.github.com>
2023-05-19 14:01:48 +0200

    Fixed iterator implementation

--- include/string/string.hpp
+++ include/string/string.hpp
@@ -18,37 +18,46 @@
 #include <stdexcept>   // std::out_of_range, std::length_error.
 #include <cassert>
 #include <sstream>
+#include <iterator>
 
 namespace detail {
     class string{
     public:
         using iterator = char*;
-		using const_iterator = const char*;
+        using const_iterator = const char*;
+        using value_type = char;
+        using pointer = char*;
+        using const_pointer = char*;
+        using reference = char&;
+        using const_reference = const char&;
 		using reverse_iterator = std::reverse_iterator<iterator>;
 		using const_reverse_iterator = std::reverse_iterator<const_iterator>;
     public:
         string();
         string(const string& rhs);
+        string(const std::string& rhs);
         explicit string(const char* cstr);
         string(string&& rhs) noexcept;
+        string(std::string&& rhs) noexcept;
         string(std::initializer_list<char> list);
         ~string();
 
-        iterator begin();
-        iterator end();
-        const_iterator cbegin() const;
-        const_iterator cend() const;
-        reverse_iterator rbegin();
-        const_reverse_iterator crbegin() const noexcept;
-        reverse_iterator rend();
-        const_reverse_iterator crend() const noexcept;
+		iterator begin() noexcept { return m_buffer; };
+		iterator end() noexcept { return (m_buffer + m_size); };
+		const_iterator cbegin() const noexcept { return m_buffer; };
+		const_iterator cend() const noexcept { return m_buffer + m_size; };
+
+		reverse_iterator rbegin() { return reverse_iterator(end()); }
+		reverse_iterator rend() { return reverse_iterator(begin()); }
+        const_reverse_iterator crbegin() const noexcept { return const_reverse_iterator(cend()); }
+		const_reverse_iterator crend() const noexcept { return const_reverse_iterator(cbegin()); }
 
         static constexpr auto npos          { static_cast<size_t>(-1) };
         size_t size() const noexcept        { return m_size; }
         size_t capacity() const noexcept    { return m_capacity; }
         size_t max_size() const noexcept    { return std::numeric_limits<size_t>::max(); }
-        char* data()                        { return m_str; }
-        const char* data() const            { return m_str; }
+        char* data()                        { return m_buffer; }
+        const char* data() const            { return m_buffer; }
         const char* c_str() const noexcept;
 
         char& at(size_t i) noexcept{}
@@ -61,8 +70,8 @@ namespace detail {
         void reserve(const size_t){}
         void shrink_to_fit();
         auto index(const char c) const;
-        char& operator[](size_t i) noexcept { return m_str[i]; }
-        const char& operator[](size_t i) const noexcept { return m_str[i]; }
+        char& operator[](size_t i) noexcept { return m_buffer[i]; }
+        const char& operator[](size_t i) const noexcept { return m_buffer[i]; }
 
         string& capitalize();
         string& lower();
@@ -78,25 +87,36 @@ namespace detail {
         constexpr bool contains(const string& s) const noexcept;
         constexpr bool contains(const char* s) const noexcept;
 
-        constexpr bool starts_with(const string& str) const noexcept {
-            for(auto first = this->cbegin(), second = str.cbegin();
-                first != this->cend() && second != str.cend(); 
-                ++first, ++second)
-            {
-                
-            }
-        }
-        
         constexpr bool starts_with(const char* str) const noexcept
         {
             const auto len = length(str);
             auto n = 0, y = 0;
-            for(auto iter = this->cbegin(); iter != this->cend(); ++iter, ++n){
+            for(auto iter = cbegin(); iter != cend(); ++iter, ++n){
                 if(*iter != str[n]) return false;
                 if(*iter == str[n]) ++y;
                 if(n == len) break; 
             }
             return (y == len);
+        }
+
+        constexpr bool starts_with(const string& str) const noexcept {
+            for(auto first = cbegin(), second = str.cbegin();
+                first != cend() && second != str.cend(); 
+                ++first, ++second)
+            {
+                if(*first != *second) return false;
+            }
+        }
+
+        constexpr bool starts_with(const std::string& str) const noexcept {
+            auto first = cbegin();
+            auto& second = str.cbegin();
+            
+            for(; first != cend() && second != str.cend(); 
+                ++first, ++second)
+            {
+                if(*first != *second) return false;
+            }
         }
         
         constexpr bool ends_with(const string& s) const noexcept;
@@ -104,7 +124,7 @@ namespace detail {
         constexpr bool ends_with(const char* str) const noexcept{
             const auto len = length(str);
             auto n = 0, y = 0;
-            for(auto iter = this->crbegin(); iter != this->crend(); ++iter, ++n){
+            for(auto iter = crbegin(); iter != crend(); ++iter, ++n){
                 if(*iter != str[n]) return false;
                 if(*iter == str[n]) ++y;
                 if(n == len) break; 
@@ -121,7 +141,7 @@ namespace detail {
         friend bool operator==(const string& lhs, const string& rhs);
         friend bool operator!=(const string& lhs, const string& rhs);
     private:
-        char* m_str = nullptr;
+        char* m_buffer = nullptr;
         size_t m_size = 0;
         size_t m_capacity = 0;
         void reallocate(size_t n_capacity);
