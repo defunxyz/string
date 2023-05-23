b58e04d0217845a52051d3dee33ab5142c0a9112
Fisnik <25180667+defunxyz@users.noreply.github.com>
2023-05-23 13:04:56 +0200

    Update string.hpp

--- include/string/string.hpp
+++ include/string/string.hpp
@@ -2,12 +2,6 @@
 #ifndef STRING_HPP
 #define STRING_HPP
 
-#if defined(_MSC_VER) ||                                            \
-    (defined(__GNUC__) && (__GNUC__ == 3 && __GNUC_MINOR__ >= 4) || \
-     (__GNUC__ >= 4))
-#pragma once
-#endif
-
 #define STRING_VERSION ""
 #define STRING_VERSION_MAJOR 0
 #define STRING_VERSION_MINOR 0
